c1b915e7c528cd4a83d5bfca371a229d03f3393c
Alemhar <alemhar@gmail.com>
2022-03-13 11:44:16 +0800

    Passed BookReservationTest

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function store(){
-        
+    public function store(Request $request){
+        // TODO: Validation
+
+        Book::create([
+            'title' => $request->title,
+            'author' => $request->author
+        ]);
     }
 }
