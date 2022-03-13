314df33886afedafe311bed469667c65c8eb9587
Alemhar <alemhar@gmail.com>
2022-03-13 12:57:17 +0800

    Refactor Book Validation

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,26 +10,19 @@ class BookController extends Controller
     public function store(Request $request){
         
         $data = $this->validateBook($request);
-        // $data = $request->validate([
-        //     'title' => 'required',
-        //     'author' => 'required'
-        // ]);
-
         Book::create($data);
     }
 
     public function update(Request $request, Book $book){
         $data = $this->validateBook($request);
-        
         $book->update($data);
     }
 
     protected function validateBook($request){
-        $data = $request->validate([
+        return $request->validate([
             'title' => 'required',
             'author' => 'required'
         ]);
 
-        return $data;
     }
 }
